d93510a99383baaa624cdd6c31205b89f25621e1
Rp <rudrapratapsingh@cedcoss.com>
2017-02-22 17:20:51 +0530

    Initial Commit

---
+++ testView/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  testView
+//
+//  Created by CEDCOSS Technologies Private Limited on 17/02/17.
+//  Copyright © 2017 CEDCOSS Technologies Private Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       UserDefaults.standard.register(defaults: ["UserAgent": "LALALAAALAL"])
+        // Do any additional setup after loading the view, typically from a nib.
+        let request = NSMutableURLRequest(url: URL(string: "http://demo.cedcommerce.com/magento/mobiapp/mobi/vendorapi/index/link")!)
+        request.setValue("HHGhsffsahfsd", forHTTPHeaderField: "Sud")
+        webView.loadRequest(request as URLRequest)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
