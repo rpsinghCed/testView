f17a1028d287149d24964b584c830e897b85cd10
Rp <rudrapratapsingh@cedcoss.com>
2017-02-22 17:25:08 +0530

    test Intial Commit

--- testView/ViewController.swift
+++ testView/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var webView: UIWebView!
+    
+    //ViewDid load fuction
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        UserDefaults.standard.register(defaults: ["UserAgent": "LALALAAALAL"])
